437491503d1eb23b16b644cad39d39e018a32152
Mikhail <mihail.kuznetsov.99@mail.ru>
2021-09-15 17:49:33 +0300

    info block

--- dist/index.js
+++ dist/index.js
@@ -1,6 +1,19 @@
-const btn = document.getElementById('btn')
-
+const btn = document.getElementById('btn');
+const infoBtn = document.getElementById('rules');
+const blockInfo = document.getElementById('blockInfo');
+const closeBtn = document.getElementById('closeBtn');
+const overlay = document.getElementById('overlay');
 
 btn.addEventListener('click', () => {
     console.log('Button clicked')
+});
+
+infoBtn.addEventListener('click', () => {
+    blockInfo.style.display = 'block';
+    overlay.style.display = 'flex';
+});
+
+closeBtn.addEventListener('click', () => {
+    blockInfo.style.display = 'none';
+    overlay.style.display = 'none';
 });
